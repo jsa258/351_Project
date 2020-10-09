b680b6a2cac24b8a4b18bba6d07dd14447f7a9af
r2sketch <72230653+r2sketch@users.noreply.github.com>
2020-10-09 13:24:58 -0700

    Register php change

--- register.php
+++ register.php
@@ -1,55 +1,11 @@
-<!DOCTYPE HTML>  
+<!DOCTYPE HTML>
 <html>
 <head>
-  
-<link rel="stylesheet" href="css/home.css">
-  <link rel="stylesheet" href="css/main.css">
 <style>
-
 .error {color: #FF0000;}
-.form_input input{
-  border: 1px solid lightgray;
-}
-
-.form_input input:focus, input:hover, input:active, .column3 .btn:hover {
-  border: 1px solid black;
-}
 </style>
 </head>
-<body>  
-
-  <!-- NAVIGATION STARTS -->
-
-  <nav>
-    <!--burger is the icon for the drop down menu-->
-    <div class="burger">
-      <div class="line1">
-      </div>
-      <div class="line2">
-      </div>
-      <div class="line3">
-      </div>
-    </div>
-
-    <div class="logo">
-      <a href="landing.html">
-        <header>XGAMES</header>
-      </a>
-    </div>
-
-    <ul class="nav-links">
-      <li><a href="">Home</a></li>
-      <li><a href="">Products</a></li>
-      <li><a href="">Login</a></li>
-    </ul>
-
-
-
-  </nav>
-  <!--link to script needed to create the drop down effect-->
-  <script src="js/responsive.js"></script>
-
-  <!-- NAVIGATION ENDS -->
+<body>
 
 <?php
 // Reference from https://www.w3schools.com/php/php_form_complete.asp
@@ -62,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   } else {
     $name = test_input($_POST["name"]);
   }
-  
+
   if (empty($_POST["email"])) {
     $emailErr = "Email is required";
   } else {
@@ -74,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   } else {
     $number = test_input($_POST["number"]);
   }
-  
+
   if (empty($_POST["password"])) {
     $passwordErr = "Password is required";
   } else {
@@ -84,13 +40,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 //Saving data to textfile https://www.dummies.com/programming/php/how-to-write-a-basic-text-file-in-php-for-html5-and-css3-programming/
 $saveData = <<< HERE
-name: $name
-email: $email
-phone: $number
-password: $password
+$email|$password|$name|$number
 
 HERE;
- $fp = fopen("login.txt", "a");
+ $fp = fopen("data.txt", "a");
  fwrite($fp, $saveData);
  fclose($fp);
 
@@ -98,64 +51,26 @@ function test_input($data) {
   return $data;
 }
 ?>
-<div class="center">
-<h2>Register</h2><br>
+
+<h2>Register Page</h2>
 <p><span class="error"></span></p>
-<form method="post" class="form_input center">  
-  Name <br> <input type="text" name="name" value="<?php echo $name;?>">
+<form method="post">
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
   <span class="error">* <?php echo $nameErr;?></span>
   <br><br>
-  E-mail <br><input type="text" name="email" value="<?php echo $email;?>">
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
   <span class="error">* <?php echo $emailErr;?></span>
   <br><br>
-  Phone number <br> <input type="text" name="number" value="<?php echo $number;?>">
+  Phone number: <input type="text" name="number" value="<?php echo $number;?>">
   <span class="error">* <?php echo $numberErr;?></span>
   <br><br>
-  Password <br> <input type="text" name="password" value="<?php echo $password;?>">
+  Password: <input type="text" name="password" value="<?php echo $password;?>">
   <span class="error">* <?php echo $passwordErr;?></span>
-  <br><br> 
+  <br><br>
 
-  <input type="submit" name="submit" value="Submit">  
+  <input type="submit" name="submit" value="Submit">
 </form>
 
-</div>
- <!--begin footer-->
- <div class="footer">
-   <div class="inner-footer">
-
-     <div class="footer-items">
-       <h5> XGames </h5>
-       <p> Description of the website</p>
-     </div>
-     <div class="footer-items">
-       <div class="contact">
-         <h6> Contact Us </h6>
-         <span><i class="fas fa-envelope"></i> info@gmail.com</span>
-         <i class="fas fa-phone"></i>604-232-3421
-       </div>
-     </div>
-
-     <div class="footer-items">
-       <div class="social">
-         <h6> Social Media </h6>
-         <a href="https://facebook.com"><i class="fab fa-facebook"></i></a>
-         <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
-         <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
-       </div>
-     </div>
-
-     <div class="footer-items">
-       <h6> Quick Links </h6>
-       <ul>
-         <li><a href="">Home</a></li>
-         <li><a href="">Products</a></li>
-       </ul>
-     </div>
-   </div>
-   <div class="footer-bottom">
-     Copyright &copy;
-   </div>
- </div>
 
 </body>
 </html>
