7031cd9a3731762f92ddf9a2f7fcb510df9b911a
Michelle <michelle725s@hotmail.com>
2020-10-09 20:47:52 -0700

    updated login for products and detail

--- detailpage.php
+++ detailpage.php
@@ -30,8 +30,22 @@
     <ul class="nav-links">
       <li><a href="index.php">Home</a></li>
       <li><a href="products.php">Products</a></li>
-      <li><a href="login.php">Login</a></li>
-    </ul>
+      <?php
+        $file = 'user.txt';
+        if($handle = fopen($file, 'r')) { // read this Hello World! from filetest.txt
+           // fill in your own code. Hint! each character is 1 byte
+            $content = fread($handle,12);
+            fclose($handle);
+        }
+        if(trim(file_get_contents('user.txt')) == false){
+
+         echo "<li><a href=\"login.php\">Login</a></li>";
+        }else{
+          echo "Welcome  $content";
+          echo "<li><a href=\"logout.php\">Logout</a></li>";
+        }
+
+      ?>    </ul>
   </nav>
 
 
