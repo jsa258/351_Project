ae9420dcc1f67d74372ed34bdbc6ad6af01482ba
r2sketch <72230653+r2sketch@users.noreply.github.com>
2020-12-03 06:57:27 -0800

    AJAX jQuery product filtering functional

--- getProductData.php
+++ getProductData.php
@@ -1,30 +1,52 @@
 <?php
-session_start();
-require 'connection.php'; //connect to database
-$result_array = array();
-$condition_arr = array();
-$selectProduct = "SELECT * FROM games WHERE games.platform IN";
+    $host         = "localhost";
+    $username     = "root";
+    $password     = "";
+    $dbname       = "iat352_project";
 
-if (!empty($_POST['categories'])) {
-  array_push($condition_arr, "'" . $_POST["categories"] . "'");
-  }
+    $result_array = array();
+    /* Create connection */
+    $conn = new mysqli($host, $username, $password, $dbname);
 
-if (count($condition_arr) > 0){
-  $condition_str = implode(",", $condition_arr);
-  $selectProduct .=  "(" . $condition_str . ") LIMIT 15";
-  // $selectProduct .= "('Wii') LIMIT 5";
-}
-
-$result = mysqli_query($connection, $selectProduct);
-
-$results_rows = mysqli_num_rows($result);
-
-if ($results_rows > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        array_push($result_array, $row);
+    /* Check connection  */
+    if ($conn->connect_error) {
+         die("Connection to database failed: " . $conn->connect_error);
     }
-}
+    /* SQL query to get results from database */
+    $info = $_POST['info'];
 
-echo json_encode($result_array);
 
-?>
+    // Converting the array to comma separated string
+    $info = implode(",",$info);
+
+    $sql = "SELECT * FROM games WHERE games.platform IN (".$info.")";
+    if( isset($_POST['genres']) ){
+      $genres = $_POST['genres'];
+      $genres = implode(",",$genres);
+      $sql .= "AND games.genre IN (".$genres.")";
+    }
+    if( isset($_POST['publisher']) ){
+      $publisher = $_POST['publisher'];
+      $publisher = implode(",",$publisher);
+      $sql .= "AND games.publisher IN (".$publisher.")";
+    }
+    if( isset($_POST['rating']) ){
+      $rating = $_POST['rating'];
+      $rating = implode(",",$rating);
+      $sql .= "AND games.ESRB_rating IN (".$rating.")";
+    }
+
+    $sql .= "LIMIT 20";
+
+    // $sql .= "('" . $_GET["info"] . "')";
+    $result = $conn->query($sql);
+    /* If there are results from database push to result array */
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            array_push($result_array, $row);
+        }
+    }
+    /* send a JSON encded array to client */
+    echo json_encode($result_array);
+
+    $conn->close();
