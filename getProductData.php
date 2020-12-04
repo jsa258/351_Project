cad477a19684abf57f38bac1645d85b39a48b68e
r2sketch <72230653+r2sketch@users.noreply.github.com>
2020-12-04 04:40:18 -0800

    Change cursor for pages

--- getProductData.php
+++ getProductData.php
@@ -106,7 +106,7 @@
       }
       for($i=1; $i<=3; $i++)
       {
-           $output .= "<span class='pagination_link' id='".$i."'>".$i."</span>";
+           $output .= "<span class='pagination_link' style='cursor: pointer;'id='".$i."'>".$i."</span>";
       }
       $output .= '</div>';
     }
